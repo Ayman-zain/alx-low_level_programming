07bf79bf66fb6ac729d8e0ea096a4b5e7b3a826b
Ayman-zain <aymnzyn85@gmail.com>
2023-02-17 14:05:24 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main
- * return: 0
- * */
+ * main - start
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 char test[] = "\"Programming is like building a multilingual puzzle";
 puts(test);	
-return(0);
+return (0);
 }
