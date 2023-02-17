1636f758ff7dc88679ade154f068ffc20dc1d889
Ayman-zain <aymnzyn85@gmail.com>
2023-02-17 13:08:07 -0800

    puts functions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	char test[] = "\"Programming is like building a multilingual puzzle\n";
+	char test[] = "\"Programming is like building a multilingual puzzle";
 	puts(test);
         return(0);
 }
