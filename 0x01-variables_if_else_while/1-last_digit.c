26f76bf2678d90612c5bf654b0b2fdb1f20ef106
Ayman-zain <aymnzyn85@gmail.com>
2023-02-18 07:34:33 -0800

    had to fix it because of old compiler

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 	int n;
-
+	int LastDigit;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int LastDigit = n%10;
+	LastDigit = n%10;
 	if (LastDigit > 5)
 	printf("Last digit of %d is %d and is greater than 5\n", n, LastDigit);
 	else if (LastDigit == 0)
