a81a63071092edb467775277839afef921c85d15
Ayman-zain <aymnzyn85@gmail.com>
2023-02-18 08:36:57 -0800

    for loop

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,14 +5,13 @@
  * Return: Always 0 (success)
  */
 
-int main(void){
+int main(void)
+{
 
 int ch;
 
 for (ch = 'a'; ch <= 'z'; ch++)
-{
 	putchar(ch);
-	putchar('\n');
-}
+putchar('\n');
 return (0);
 }
