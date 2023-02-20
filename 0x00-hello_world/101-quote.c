fd024b1bbd01186efdcc31ac08ce58a2c4640fba
Ayman-zain <aymnzyn85@gmail.com>
2023-02-20 14:33:26 -0800

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - start
+ * Return: Always 0 (success)
+ */
+
+int main (void)
+{
+write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+return (1);
+}
