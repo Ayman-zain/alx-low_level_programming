f25c5d8cf2149392030a22b0910aefa20e272ac5
Ayman-zain <aymnzyn85@gmail.com>
2023-02-20 13:46:00 -0800

    variale size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ long int longType;
 char charType;
 
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-printf("Size of a int: %zu byte(s)\n", sizeof(intType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(doublelongType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
