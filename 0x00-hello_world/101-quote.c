a3abac1ebefeed01ab9ef793fb93a303e090d3f3
Ayman-zain <aymnzyn85@gmail.com>
2023-02-20 15:13:00 -0800

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,9 @@
  * Return: Always 0 (success)
  */
 
-int main (void)
+int main(void)
 {
-fprintf( stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
