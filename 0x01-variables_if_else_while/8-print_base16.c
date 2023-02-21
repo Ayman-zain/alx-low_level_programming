dd1fd5d757a2d8e6151af037e07e24eb01337b85
Ayman-zain <aymnzyn85@gmail.com>
2023-02-20 18:01:15 -0800

    print hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - print hexa numbers with putchar
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int digi;
+char ch;
+
+for (digi = 0; digi <= 9; digi++)
+        putchar('0' + digi);
+for (ch = 'a'; ch <= 'f'; ch++)
+        putchar(ch);
+putchar('\n');
+return (0);
+}
