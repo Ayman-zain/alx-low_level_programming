5d362c8c386568f5c936fb8a9e559893ff05c435
Ayman-zain <aymnzyn85@gmail.com>
2023-02-20 18:02:40 -0800

    print hex

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,9 @@ int digi;
 char ch;
 
 for (digi = 0; digi <= 9; digi++)
-        putchar('0' + digi);
+	putchar('0' + digi);
 for (ch = 'a'; ch <= 'f'; ch++)
-        putchar(ch);
+	putchar(ch);
 putchar('\n');
 return (0);
 }
