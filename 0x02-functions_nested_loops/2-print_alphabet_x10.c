5e5e858ecca30d0ced8b99bc977acb05dc33147c
Ayman-zain <aymnzyn85@gmail.com>
2023-02-22 12:15:18 -0800

    print alphabet 10x times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet 10 times
+ * Return:void
+ */
+
+void print_alphabet_x10(void)
+{
+char i;
+char mult;
+
+for (mult = 1; mult <= 10; mult++)
+{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+return;
+}
