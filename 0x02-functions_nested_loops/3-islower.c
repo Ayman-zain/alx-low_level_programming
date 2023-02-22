07f3da6045e3ddf8f7310acbbbe784a0799f757d
Ayman-zain <aymnzyn85@gmail.com>
2023-02-22 15:45:29 -0800

    check fotr letters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _islower - check for lower case letters
+ * _islower - check for lower case letters
  * @c: the character
  * Return:0 or 1
  */
@@ -10,7 +10,11 @@ int _islower(int c)
 {
 
 if (c >= 97 && c <= 122)
+{
 	return(1);
+}
 else
+{
 	return(0);
+}
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * int _isalpha - check for any letters
- * @c: the character
- * Return:0 or 1
+ * _isalpha - check for any letters
+ * @c: the character we need to check
+ * Return: 0 or 1
  */
 
 int _isalpha(int c)
 {
 
 if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
-	return(0);
-else
 	return(1);
+else
+	return(0);
 }
