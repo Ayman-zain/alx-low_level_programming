f451f35eb3c96be6333d09a85423a40b1b6c660b
Ayman-zain <aymnzyn85@gmail.com>
2023-02-22 15:12:32 -0800

    check for Uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int _isalpha - check for uppercase letters
+ * Return:0 or 1
+ */
+
+int _isalpha(int c)
+{
+
+if (c >= 65 && c <= 90)
+	return(0);
+else
+	return(1);
+}
