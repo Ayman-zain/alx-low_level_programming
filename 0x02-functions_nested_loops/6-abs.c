8855227e6078dd631d75f9dc83e1b3aa96b434ec
Ayman-zain <aymnzyn85@gmail.com>
2023-02-22 16:58:43 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - return abslute value
+ * @r: number to be converted
+ * Return: int number
+ */
+
+int _abs(int r)
+{
+int ABS;
+
+ABC = r * r;
+ABS = ABS / ABS;
+return (ABS)
+}
