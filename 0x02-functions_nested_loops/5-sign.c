4f0e3c76dd4164fb313d27ed7d9cdaa4fe124acf
Ayman-zain <aymnzyn85@gmail.com>
2023-02-22 16:41:24 -0800

    check sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - print sign of number
  * @n: the character we need to check
- * Return: 1 if bigger than Zero 
+ * Return: 1 if bigger than Zero
  * and 0 if its Zero and print -1 if number less than 0
  */
 
@@ -12,17 +12,17 @@ int print_sign(int n)
 
 if (n > 0)
 {
-	return (1);
 	_putchar(43);
+	return (1);
 }
 else if (n == 0)
 {
-	return (0);
 	_putchar(48);
+	return (0);
 }
 else
 {
-	return (-1);
 	_putchar(45);
+	return (-1);
 }
 }
