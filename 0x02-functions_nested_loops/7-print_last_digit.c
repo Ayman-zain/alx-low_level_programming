aa23a2c11a72561ee1646db69c6fd55aa266f472
Ayman-zain <aymnzyn85@gmail.com>
2023-02-22 17:28:05 -0800

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,14 @@ int print_last_digit(int r)
 {
 int Last = 0;
 
+if (r < 0)
+	r = r * -1;
 if (r > 9)
 {
 Last = r % 10;
+_putchar(Last + '0');
 return (Last);
 }
+_putchar(r + '0');
 return (r);
 }
