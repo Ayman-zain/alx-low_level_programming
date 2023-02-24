e02865ece825f2f3ff7fff09d449fe4902bec309
Ayman-zain <aymnzyn85@gmail.com>
2023-02-24 09:38:45 -0800

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - check for lower case letters
+ * @c: the character
+ * Return:0 or 1
+ */
+
+int _islower(int c)
+{
+
+if (c >= 65 && c <= 90)
+{
+        return (1);
+}
+else
+{
+        return (0);
+}
+}
