0fd0bd0127dc70132b221e181b68988707095767
Ayman-zain <aymnzyn85@gmail.com>
2023-02-24 10:56:09 -0800

    from 0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 to 14
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+int digi;
+int i;
+
+for (i = 0; i < 10; i++)
+{
+for (digi = 0; digi <= 14; digi++)
+	 _putchar('0' + digi);
+_putchar('\n');
+}
+_putchar('\n');
+}
