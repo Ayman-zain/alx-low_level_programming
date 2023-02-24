cccc6887b87668b6e66733008a82998da90e41fe
Ayman-zain <aymnzyn85@gmail.com>
2023-02-24 10:19:16 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print single digit numbers from 0 to 9
+ * Return: Always 0 (success)
+ */
+
+void print_numbers(void)
+{
+int digi;
+
+for (digi = 0; digi <= 9; digi++)
+	_putchar('0' + digi);
+_putchar('\n');
+return (0);
+}
