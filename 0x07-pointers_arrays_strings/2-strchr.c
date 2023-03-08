1cf06de222db16d0739f2551972668fca6542e6a
Ayman-zain <aymnzyn85@gmail.com>
2023-03-07 18:15:26 -0800

    check fo similarity

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
+ * @s: string to be searched at.
+ * @c: desired character.
+ * Return: int 0.
  */
 char *_strchr(char *s, char c)
 {
