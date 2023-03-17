7366f4997a1b98288d153959c1a1a856615aa620
Ayman-zain <aymnzyn85@gmail.com>
2023-03-16 17:28:57 -0700

    arr

---
+++ 0x0C-more_malloc_free/_memset.c
@@ -0,0 +1,26 @@
+#include <stdlib.h
+#include "main.h"
+
+
+char *_memset(char *s, char b, unsigned int n)
+		
+{
+		
+	unsigned int i;
+		
+
+		
+	for (i = 0; i < n; i++)
+		
+	{
+		
+		s[i] = b;
+		
+	}
+		
+
+		
+	return (s);
+		
+}
+
