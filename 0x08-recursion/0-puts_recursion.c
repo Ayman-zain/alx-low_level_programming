af4043bdf3dac16be2838d94b34571cb227d1b7c
Ayman-zain <aymnzyn85@gmail.com>
2023-07-05 18:02:57 -0700

    oooooo

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,14 +6,15 @@
  *
  *
  */
-int i = 0;
-void _puts_recursion(char *s){
-	if(s[i] == '\0'){
-		_putchar('\n');
+void _puts_recursion(char *s)
+	{
+	if (*s == '\0')
+	{
+		_putchar ('\n');
 	}
-	else{
-		_putchar(s[i]);
-		i++;
-		_puts_recursion(s);
+	else
+	{
+		_putchar (*s);
+		_puts_recursion(s + 1);
 	}
 	}
