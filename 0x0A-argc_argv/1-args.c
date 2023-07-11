872867d169ff06ce7a223dbec2800696a156ba5a
Ayman-zain <aymnzyn85@gmail.com>
2023-07-11 14:15:57 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print th program's name
+ * @argc: number of arguments to main.
+ * @argv: an array of strings that include args to main.
+ * Return: 0 if everything worked fine
+ */
+
+int main(int argc, char __attribute__((__unused__)) **argv)
+{
+printf("%d\n", argc);
+return (0);
+}
