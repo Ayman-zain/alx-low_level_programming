1882ef7106fc022a5b624ca7a10aa19034cf8683
Ayman-zain <aymnzyn85@gmail.com>
2023-07-18 19:01:15 -0700

    doog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOGGY_H
+#define DOGGY_H
+
+/**
+ * struct dog - a struct that stores a dog's name, age and owner's name.
+ * @name: the dog's name.
+ * @age: the dog's age.
+ * @owner: the owner's name;
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
