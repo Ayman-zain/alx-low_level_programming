ad4dc0f3a3fb21f6317f388ec3f7c712f0df9c1d
Ayman-zain <aymnzyn85@gmail.com>
2023-09-02 05:46:27 +0300

    h

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * get_bit - a function that returns the value of a bit at a given index.
+ * set_bit - a function that sets the value of a bit to 1 at a given index. 
  * @n: number given.
  * @index: the index
  * Return:  value of the bit at index or -1 if an error occured
