4795a2214c2b3172d6641d2d66c8c2e60b7cfa9a
Ayman-zain <aymnzyn85@gmail.com>
2024-02-08 21:20:39 +0200

    1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * TwoPow - return 2 to power of p.
+ * @p: power of 2
+ * Return: int
+ */
+int TwoPow(int p)
+{
+	int r = 1;
+
+	while (p--)
+		r = r * 2;
+return (r);
+}
+/**
+ * binary_to_uint - function that sets the value of a bit to 1 at given index.
+ * @b: binary number stored in char array
+ * Return: unsigned int.
+ *
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+int size = 0, i, pow, dec = 0;
+if (b == NULL)
+{
+	return (0);
+}
+while (b[size] != '\0')
+{
+	if (b[size] != '0' || b[size] != '1')
+		return (0);
+	size++;
+}
+size++;
+pow = size - 2;
+for (i = 0; i < size; i++)
+{
+	if (b[i] == '1')
+		dec = dec + TwoPow(pow);
+	pow--;
+}
+return (dec);
+}
